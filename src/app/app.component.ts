84f9edfc16e9ebf0cf043dec0c535e810c5e1d44
Adheli Tavares <adt@certi.org.br>
2018-01-24 09:02:24 -0400

    ngx translate configurado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = 'Hugh';
+
+  constructor(translate: TranslateService) {
+    let currentLang = translate.getBrowserLang();
+
+    if (currentLang.includes('en')) {
+      currentLang = 'en-US';
+    } else {
+      currentLang = 'pt-BR';
+    }
+
+    translate.setDefaultLang(currentLang);
+    localStorage.setItem('currentLang', currentLang);
+  }
 }
